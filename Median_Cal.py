32b80a9010786dcb0e73eaeecb1d7df891697431
Scantlin <scantlincayson370@gmail.com>
2024-01-05 21:17:37 +0800

    On branch main  Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   Median_Cal.py

--- Median_Cal.py
+++ Median_Cal.py
@@ -1,10 +1,20 @@
-def mid(mo):
-    count = len(mo)
-    if (count % 2) != 0:
-        for i,c in enumerate(mo):
-            if i == (((len(mo) -1)/2)):
-                return(c)
-    else:
-        print("")
+class median:
+    def mid(self, mo):
+        self.mo = mo
+        count = len(mo)
+        if (count % 2) != 0:
+            for i,c in enumerate(self.mo):
+                if i == (((len(self.mo) -1)/2)):
+                    return f"The median is {(c)}"
+        else:
+            return f"No median: {len(self.mo)}"
         
-print(mid("1 2 3 4 5"))
+how_many =int(input("how many numbers in median: "))        
+med = []
+print("Enter all numbers: ")
+for i in range(how_many):
+    me = int(input())
+    med.insert(i, me)
+
+xe = median()
+print((xe.mid(med)))
